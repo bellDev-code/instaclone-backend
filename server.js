f3ebac0b3dead4bc1d92236d0f995283f57e5acf
bellDev-code <sumaoo20@naver.com>
2022-07-18 23:17:32 +0900

    Add Prisma Client

--- server.js
+++ server.js
@@ -1,43 +1,57 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server"
 
+
+const client = new PrismaClient()
+
+// Type Definitions가 Prisma와 GraphQL로 작업할때 유일하게 신경써줘야한다.
+// Type Definitions와 Schema를 일치시키는게 중요하다.
 const typeDefs = gql`
-    type Movie {
-        id: Int
-        title: String
-        year: Int
-    }
-    type Query {
-        movies: [Movie]
-        movie: Movie
-    }
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
     type Mutation {
-        createMovie(title:String!): Boolean
-        deleteMovie(title:String!): Boolean
+      createMovie(title:String!, year:Int!, genre: String): Movie
+      deleteMovie(title:String!): Boolean
     }
 `;
 
 const resolvers = {
-    Query: {
-        movies: () => [],
-        movie: () => ({title: "Hello", year: 2022})
-    },
-    Mutation: {
-        createMovie: (_, {title}) => {
-            console.log(title)
-            return true
+  Query: {
+    // 이것만으로 우리의 데이터베이스로 가서 모든 영화들을 검색하게 된다.
+    movies: () => client.movie.findMany(),
+    movie: () => ({title: "Hello", year: 2022})
+  },
+  Mutation: {
+    createMovie: (_, {title, year, genre}) => 
+      client.movie.create({ 
+        data: {
+          title,
+          year,
+          genre,
         },
-        deleteMovie: (_, {title}) => {
-            console.log(title)
-            return true
-        }
+      }),
+    deleteMovie: (_, {title}) => {
+      console.log(title)
+      return true
     }
+  }
 }
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers
 })
 
 server
-    .listen()
-    .then(() => console.log("Server is running on http://loaclhost:4000/"))
+  .listen()
+  .then(() => console.log("Server is running on http://loaclhost:4000/"))
