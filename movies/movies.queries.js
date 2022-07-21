123097a4b909ef8ef5bdb0d5d36a4f160dfa1cb7
bellDev-code <sumaoo20@naver.com>
2022-07-21 23:16:16 +0900

    setting instaclone

--- movies/movies.queries.js
+++
@@ -1,9 +0,0 @@
-import client from "../client";
-
-export default {
-    Query: {
-        // 이것만으로 우리의 데이터베이스로 가서 모든 영화들을 검색하게 된다.
-        movies: () => client.movie.findMany(),
-        movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-      },
-}
